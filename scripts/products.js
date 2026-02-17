bbd5ebd1770eb6efe0554aa5d915f95b32a51a83
Shajnin <shajninhuda17@email.com>
2026-02-17 11:54:50 -0500

    dynamically activated all products button

--- scripts/products.js
+++ scripts/products.js
@@ -10,6 +10,7 @@ const displayCategories = (categories) =>{
    const firstBtn = document.createElement('button');
    firstBtn.classList = "btn btn-primary btn-outline rounded-2xl";
    firstBtn.innerText = 'All';
+   firstBtn.addEventListener('click',loadAllProducts)
    categoryContainer.append(firstBtn)
    categories.forEach(category => {
    const btn = document.createElement('button');
@@ -18,4 +19,40 @@ const displayCategories = (categories) =>{
    categoryContainer.append(btn)
    });
 }
-loadCategories()
+const loadAllProducts = ()=>{
+    fetch('https://fakestoreapi.com/products')
+     .then(res=>res.json())
+     .then(data=>displayAllProducts(data))
+}
+const displayAllProducts = (products) =>{
+    const allProductsContainer = document.getElementById('all-products-container')
+    allProductsContainer.innerHTML = "";
+    products.forEach(product=>{
+        const card = document.createElement('div');
+        card.innerHTML = `
+          <div class="card bg-base-100 h-full shadow-sm">
+                    <figure class="bg-slate-100 p-6">
+                        <img class="h-56 w-full object-contain" src="${product.image}" />
+                    </figure>
+                    <div class="card-body">
+                        <div class="flex justify-between items-center">
+                        <button class="btn text-xs bg-sky-100 text-blue-600 rounded-xl">${product.category}</button>
+                        <div>
+                            <i class="fa-solid fa-star text-yellow-300"></i>
+                            <span>${product.rating.rate}</span> <span>(${product.rating.count})</span>
+                        </div>
+                        </div>
+                        <h2 class="text-xl font-bold">${product.title}</h2>
+                        <p class="font-bold text-xl">$${product.price}</p>
+                        <div class="flex justify-between text-2xl mt-6">
+                            <button class="btn p-6 rounded-xl"><i class="fa-regular fa-eye"></i>Details</button>
+                            <button class="btn bg-blue-800 text-white p-6 rounded-xl"><i class="fa-solid fa-cart-shopping "></i>Add</button>
+                        </div>
+                    </div>
+                </div>
+        `
+        allProductsContainer.append(card)
+
+    } )
+}
+loadCategories();
